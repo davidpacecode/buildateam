a7d144d6bb4fc55760fec281223afd96a2fb6838
David Pace <david.pace@hey.com>
2026-03-02 15:37:17 -0600

    fixed headshots for players by massaging player names

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,7 @@
 class Player < ApplicationRecord
+
+  # strip or swap out characters that break file names...
+  def headshot_file_name
+    "#{first_name.gsub(".","").gsub("'","").downcase}_#{last_name.gsub(".","").gsub(" ","_").downcase}.png"
+  end
 end
