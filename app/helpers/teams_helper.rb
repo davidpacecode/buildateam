70224b0db3c2d8121d2d35b1e9545233a9aa8a15
davidpacecode <davidpace@gmail.com>
2026-03-09 00:26:24 -0500

    have cards implemented w tool tips

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,10 @@
 module TeamsHelper
+
+  def card_appearance (new_team, team, pos)
+    if !(new_team || team.send("#{pos.downcase}_id") == 0)
+      "filled-outlined"
+    else
+      "outlined"
+    end
+  end
 end
